16508291ef2d23c097d9830fe24e0b69fffd3a81
Pavel.Elisseeff <pavel.eliseev@gmail.com>
2024-01-21 15:33:07 +0300

    1

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # gemini_google
 
+https://cloud.google.com/vertex-ai/docs/generative-ai/model-reference/gemini?_ga=2.217948438.-1460943777.1705824541
+
 pip install -q -U google-generativeai
+
+gcloud auth print-access-token
--- gemini_main.py
+++ gemini_main.py
@@ -1,15 +1,15 @@
-import pathlib
-import textwrap
+PROJECT_ID = "'ai-elis-project'"
+LOCATION = "us-central1" #e.g. us-central1
 
-import google.generativeai as genai
+import vertexai
+vertexai.init(project=PROJECT_ID, location=LOCATION)
+from vertexai.preview.generative_models import (
+    GenerationConfig,
+    GenerativeModel,
+)
+# Restart kernel after installs so that your environment can access the new packages
+import IPython
+import time
 
-# Used to securely store your API key
-from google.colab import userdata
-
-from IPython.display import display
-from IPython.display import Markdown
-
-
-def to_markdown(text):
-  text = text.replace('•', '  *')
-  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+app = IPython.Application.instance()
+app.kernel.do_shutdown(True)
